d1e1ebf3bf133c84ce513cdff169a7ced1d81798
marciamillanao <marcia.millanao@gmail.com>
2019-06-27 16:47:10 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,20 @@
+const STUDENTS = {
+  "student": [{
+    "name": "Alicia",
+    "lastName": "Díaz",
+    "type": "student",
+    "hobby": "Cantar" 
+  },
+  {
+    "name": "Ale",
+    "lastName": "Mendez",
+    "type": "student",
+    "hobby": "Andar en bicicleta" 
+  },
+  {
+    "name": "Rosa",
+    "lastName": "Espinoza",
+    "type": "student",
+    "hobby": "Comer" 
+  }]
+}
---
+++ index.js
@@ -0,0 +1,1 @@
+//Aca crearemos nuestro js
